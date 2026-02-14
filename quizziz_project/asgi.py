1f096ff0c2b3799c4a92dc131de077cfd0b51959
Ev <korogodin_ea@mail.ru>
2026-02-14 19:38:25 +0300

    первоначальная настройка проекта и сервера

--- quizziz_project/asgi.py
+++ quizziz_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for simple_votings project.
+ASGI config for quizziz_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_votings.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quizziz_project.settings')
 
 application = get_asgi_application()
