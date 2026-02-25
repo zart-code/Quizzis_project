3ee84789f41339fafa8175994ba1b61a58385901
Gleb Bogatyrev <glebbogatyrev765@gmail.com>
2026-02-25 22:33:27 +0300

    подключены urls

--- quizziz_project/urls.py
+++ quizziz_project/urls.py
@@ -4,10 +4,18 @@ from main.views import *
 
 """
 Структура страниц в формате x.y (где x - уровень, y - номер страницы на этом уровне):
+
+Уровень 0 - Главная
 0.1 - Главная страница (меню) main_page
+
+Уровень 1 - Аутентификация
 1.1 - Страница входа login_page
 1.2 - Страница регистрации register_page
-1.3 - Страница выхода logout
+1.3 - Страница выхода logout (редирект на main_page)
+
+Уровень 2 - Профиль пользователя
+2.1 - Страница профиля profile
+2.2 - Редактирование профиля edit_profile
 """
 
 urlpatterns = [
@@ -16,4 +24,6 @@ urlpatterns = [
     path('login/', login_page, name='login_page'),
     path('register/', register_page, name='register_page'),
     path('logout/', logout_view, name='logout'),
+    path('profile/', profile_view, name='profile'),
+    path('profile/edit/', edit_profile_view, name='edit_profile'),
 ]
