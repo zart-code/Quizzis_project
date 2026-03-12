2b2fbe0e9f4808ba3d71976e5df6e3ec95df6119
Ev <korogodin_ea@mail.ru>
2026-03-12 15:55:38 +0300

    profile debug

--- main/views_features/views_profile.py
+++ main/views_features/views_profile.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models import Avg, Max, Count, Q
 from main.forms import CustomUserCreationForm, StyledAuthenticationForm
+from main.models import Quiz, QuizResult, UserAchievement, Achievement
 
 
 @login_required(login_url='login_page')
