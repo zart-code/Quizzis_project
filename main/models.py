38bd75979db047cd4b49705bfa623d944cefd425
zart <artemon192009@gmail.com>
2026-04-09 18:57:41 +0300

    начинаю работаь над админ панелью

--- main/models.py
+++ main/models.py
@@ -5,6 +5,30 @@ import random
 import string
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    """Профиль пользователя"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    is_admin = models.BooleanField(default=False, verbose_name='Администратор')
+    is_banned = models.BooleanField(default=False, verbose_name='Заблокирован')
+
+    def __str__(self):
+        return f'Профиль {self.user.username}'
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.get_or_create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
 
 
 def generate_pin():
