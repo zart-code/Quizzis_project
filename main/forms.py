a7a87ed84bef54c4efa8cb7e26b01522b381b70b
Gleb Bogatyrev <glebbogatyrev765@gmail.com>
2026-04-15 21:29:53 +0300

    в форме регистрации теперь выбор роли. Пока что по умолчанию админом назначается пользователь с ником admin

--- main/forms.py
+++ main/forms.py
@@ -1,12 +1,22 @@
 """Формы для views"""
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django import forms
+from main.models import Profile
 
 FIELD_ATTRS = {'class': 'form-control'}
 
 
 class CustomUserCreationForm(UserCreationForm):
     """Форма пользователя"""
+    role = forms.ChoiceField(
+        choices=[
+            (Profile.TEACHER, 'Учитель'),
+            (Profile.STUDENT, 'Ученик'),
+        ],
+        label='Роль',
+    )
+
     class Meta(UserCreationForm.Meta):
         """Метаданные"""
         model = User
@@ -16,6 +26,18 @@ class CustomUserCreationForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs.update(FIELD_ATTRS)
+
+    def save(self, commit=True):
+        user = super().save(commit=commit)
+        if commit and hasattr(user, 'profile'):
+            if user.username == 'admin':
+                user.profile.role = Profile.ADMIN
+                user.profile.is_admin = True
+            else:
+                user.profile.role = self.cleaned_data['role']
+                user.profile.is_admin = False
+            user.profile.save()
+        return user
 
 
 class StyledAuthenticationForm(AuthenticationForm):
