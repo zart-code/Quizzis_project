81398d974c3fe30fbdfcaf6ae0ff84d11d063a7d
zart-code <artemon192009@gmail.com>
2026-05-30 18:02:03 +0300

    fix static

--- quizziz_project/settings.py
+++ quizziz_project/settings.py
@@ -82,7 +82,7 @@ TIME_ZONE = "Europe/Moscow"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "/static/"
+STATIC_URL = "/main/static/"
 STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [BASE_DIR / "main" / "static"]
